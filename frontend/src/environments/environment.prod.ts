94d6337ab233ae8c8b9e6572ca9fb1ebe9b0cb8c
Keshav Singh <keshavsingh3197@users.noreply.github.com>
2026-08-16 20:29:21 +0530

    Fixed api issue

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   idpUrl: 'https://admin.keshavsingh.in/api',
-  apiUrl: 'https://pratiyogita-api.onrender.com/api',
+  apiUrl: 'https://pratiyogita.onrender.com/api',
   loginUrl: 'https://admin.keshavsingh.in/login',
 };
